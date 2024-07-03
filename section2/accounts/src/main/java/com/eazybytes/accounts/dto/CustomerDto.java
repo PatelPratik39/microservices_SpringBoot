d8f5cd52613ab8770e61b782e93671206f53ca45
PatelPratik39 <ptl.pratik717@gmail.com>
2024-07-03 10:04:53 -0500

    OpenAPI Documentation configured from DTO  classes for Swagger UI

--- section2/accounts/src/main/java/com/eazybytes/accounts/dto/CustomerDto.java
+++ section2/accounts/src/main/java/com/eazybytes/accounts/dto/CustomerDto.java
@@ -1,5 +1,6 @@
 package com.eazybytes.accounts.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
@@ -8,16 +9,29 @@ import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
+@Schema(
+        name = "Customer",
+        description = "Schema to hold Customer and Account information"
+)
 public class CustomerDto {
 
+    @Schema(
+            description = "Name of The Customer" , example = "Chucha Singh"
+    )
     @NotEmpty(message = "Name can not be null or empty!!")
     @Size(min=5, max =30, message = "The length of the customer name should be between 5 and 30")
     private String name;
 
+    @Schema(
+            description = "Email of The Customer" , example = "chucha@email.com"
+    )
     @NotEmpty(message = "Name can not be null or empty!!")
     @Email(message = "Email Address should be a valid value !!")
     private String email;
 
+    @Schema(
+            description = "PhoneNumber of The Customer" , example = "1234567890"
+    )
     @Pattern(regexp = "($|[0-9]{10})", message = "Mobile number must be 10 digits")
     private String mobileNumber;
 
--- section2/accounts/src/main/java/com/eazybytes/accounts/dto/ErrorResponseDto.java
+++ section2/accounts/src/main/java/com/eazybytes/accounts/dto/ErrorResponseDto.java
@@ -1,5 +1,6 @@
 package com.eazybytes.accounts.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.http.HttpStatus;
@@ -8,11 +9,26 @@ import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
+@Schema(
+        name = "ErrorResponse",
+        description = "Schema to hold error response information"
+)
 public class ErrorResponseDto {
 
-
+    @Schema(
+            description = "API path invoked by client"
+    )
     private String apiPath;
+    @Schema(
+            description = "Error code representing the error happened"
+    )
     private HttpStatus errorCode;
+    @Schema(
+            description = "Error code representing the error happened"
+    )
     private String errorMessage;
+    @Schema(
+            description = "Error code representing the error happened"
+    )
     private LocalDateTime errorTime;
 }
