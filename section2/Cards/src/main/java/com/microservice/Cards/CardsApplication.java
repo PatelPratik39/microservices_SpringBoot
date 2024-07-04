b4beac33905fe985c027021c8a0b212b44452ac9
PatelPratik39 <ptl.pratik717@gmail.com>
2024-07-04 17:53:35 -0500

    Cards CRUD microservice works perfectly

--- section2/Cards/src/main/java/com/microservice/Cards/CardsApplication.java
+++ section2/Cards/src/main/java/com/microservice/Cards/CardsApplication.java
@@ -14,21 +14,21 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @OpenAPIDefinition(
 		info = @Info(
 				title = "Cards microservice REST API Documentation",
-				description = "EazyBank Cards microservice REST API Documentation",
+				description = "Bank App Cards microservice REST API Documentation",
 				version = "v1",
 				contact = @Contact(
-						name = "Madan Reddy",
-						email = "tutor@eazybytes.com",
-						url = "https://www.eazybytes.com"
+						name = "Pratik Patel",
+						email = "pratik@email.com",
+						url = "http://localhost:9000/swagger-ui/index.html"
 				),
 				license = @License(
 						name = "Apache 2.0",
-						url = "https://www.eazybytes.com"
+						url = "http://localhost:9000/swagger-ui/index.html"
 				)
 		),
 		externalDocs = @ExternalDocumentation(
-				description = "EazyBank Cards microservice REST API Documentation",
-				url = "https://www.eazybytes.com/swagger-ui.html"
+				description = "Bank App Cards microservice REST API Documentation",
+				url = "http://localhost:9000/swagger-ui/index.html"
 		)
 )
 public class CardsApplication {
