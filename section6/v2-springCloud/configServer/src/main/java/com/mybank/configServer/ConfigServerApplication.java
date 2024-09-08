7d6315afd1c26cf680cd2a70328e0d831a41d725
PatelPratik39 <ptl.pratik717@gmail.com>
2024-09-08 11:38:46 -0500

    Spring clou configServer

--- section6/v2-springCloud/configServer/src/main/java/com/mybank/configServer/ConfigServerApplication.java
+++ section6/v2-springCloud/configServer/src/main/java/com/mybank/configServer/ConfigServerApplication.java
@@ -9,7 +9,6 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(ConfigServerApplication.class, args);
 	}
 
